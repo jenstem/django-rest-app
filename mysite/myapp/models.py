9799bee75c4a030bf85a41f479cdf12992e3b24e
jenstem <jennifer.stemkowski@gmail.com>
2024-05-01 10:25:57 -0400

    Setup django framework + movie model

---
+++ mysite/myapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Movie(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    rating = models.FloatField()
